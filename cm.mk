b008b875b3428fce9ed1bd00dd7f850813d019d8
GruesomeWolf <boober19841126@gmail.com>
2015-02-28 00:27:01 -0500

    fixup device for vm12

--- cm.mk
+++
@@ -1,8 +0,0 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-$(call inherit-product, device/samsung/kltespr/full_kltespr.mk)
-
-PRODUCT_DEVICE := kltespr
-PRODUCT_NAME := cm_kltespr
